a3bd9c03cb1b9baa0fbfe558617b830233d52724
AdilShazia <adilmms@gmail.com>
2026-03-04 11:25:56 +0000

    Setup Django RESt framework

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-m4ju1qs_)_%#05s(&uh!k8s3+fl%j)rkehjj-jko@&#3v9%qgd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
